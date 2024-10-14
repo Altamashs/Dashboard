8ee6441ca13e9e1148ecaf0c6b89b4703ea2a8ad
Altamashs <altamashshikalgar345@gmail.com>
2024-10-14 23:50:56 +0530

    try

--- src/app/components/Calendar/Calendar.js
+++ src/app/components/Calendar/Calendar.js
@@ -1,7 +1,7 @@
 'use client'
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
-import './Calendar.css'; 
+import './Calendar.css';
 
 const Calendar = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
